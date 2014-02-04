7102f78d51782d42d3012b8761fd7c825c1d4ca1
Dmitry Marakasov <amdmi3@amdmi3.ru>
2014-02-04 08:04:36 +0400

    Add game template, currently showing some animation for Desert Strike

---
+++ src/main.cc
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2013-2014 Dmitry Marakasov
+ *
+ * This file is part of openstrike.
+ *
+ * openstrike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * openstrike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with openstrike.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+#include <SDL2pp/SDL2pp.hh>
+
+#include <dat/datfile.hh>
+#include <dat/datgraphics.hh>
+#include <graphics/spritemanager.hh>
+
+void usage(const char* progname) {
+	std::cerr << "Usage: " << progname << " <filename.dat>" << std::endl;
+}
+
+int realmain(int argc, char** argv) {
+	if (argc != 2) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	DatFile datfile(argv[1]);
+
+	SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+	SDL2pp::Window window("OpenStrike", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_RESIZABLE);
+	SDL2pp::Renderer render(window, -1, SDL_RENDERER_ACCELERATED);
+
+	render.SetDrawBlendMode(SDL_BLENDMODE_BLEND);
+
+	SpriteManager spriteman(render);
+
+	int spriteid = 0;
+	{
+		Buffer data = datfile.GetData("BOMBER");
+		DatGraphics gfx(data);
+		spriteid = spriteman.Add(gfx, 0, 3);
+	}
+
+	while (1) {
+		// Process events
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				return 0;
+			} else if (event.type == SDL_KEYDOWN) {
+				if (event.key.keysym.sym == SDLK_ESCAPE || event.key.keysym.sym == SDLK_q)
+					return 0;
+			}
+		}
+
+		// Render
+		render.SetDrawColor(0, 32, 32);
+		render.Clear();
+
+		int frameid = SDL_GetTicks() / 500 % 3;
+		spriteman.Render(spriteid + frameid, 0, 0, SpriteManager::FRAMECORNER);
+
+		render.Present();
+
+		// Frame limiter
+		SDL_Delay(1);
+	}
+
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	try {
+		return realmain(argc, argv);
+	} catch (SDL2pp::Exception& e) {
+		std::cerr << "Error: " << e.what() << " (" << e.GetSDLError() << ")" << std::endl;
+	} catch (std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+
+	return 1;
+}
+
