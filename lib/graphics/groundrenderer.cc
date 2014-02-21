8975d299d96e733fd8c40372fdbb12d98e97a135
Dmitry Marakasov <amdmi3@amdmi3.ru>
2014-02-21 16:55:14 +0400

    Draw sector grid in GroundRenderer

--- lib/graphics/groundrenderer.cc
+++ lib/graphics/groundrenderer.cc
@@ -28,4 +28,19 @@ GroundRenderer::GroundRenderer(SDL2pp::Renderer& renderer) : renderer_(renderer)
 void GroundRenderer::Render(const Game& game, const Camera& camera) {
 	renderer_.SetDrawColor(158, 126, 61);
 	renderer_.FillRect(camera.GetViewport());
+
+	// Draw sector grid
+	renderer_.SetDrawColor(255, 0, 0);
+	for (int vline = 0; vline <= game.GetWidth() / 512.0; vline++) {
+		renderer_.DrawLine(
+				camera.GameToScreen(Vector3f(vline * 512.0, 0, 0)),
+				camera.GameToScreen(Vector3f(vline * 512.0, game.GetHeight(), 0))
+			);
+	}
+	for (int hline = 0; hline <= game.GetHeight() / 512.0; hline++) {
+		renderer_.DrawLine(
+				camera.GameToScreen(Vector3f(0, hline * 512.0, 0)),
+				camera.GameToScreen(Vector3f(game.GetWidth(), hline * 512.0, 0))
+			);
+	}
 }
